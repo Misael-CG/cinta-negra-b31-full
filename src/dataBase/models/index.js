c199c96ff5f61af895c5142f7abb78f3da1a1470
anders <anderson.monroy.r79@gmail.com>
2019-07-02 00:10:25 -0500

    register post

--- src/dataBase/models/index.js
+++ src/dataBase/models/index.js
@@ -2,12 +2,14 @@
 const mongoose = require('mongoose');
 //importamos los esquemas
 const UserSchema = require('../schemas/userSchema');
+const PostSchema = require('../schemas/postSchema');
 
 // creacion de models, crea un model con el nombre de la coleccion y el esquema
 const UserModel = mongoose.model("usersCintaNegra", UserSchema);
-
+const PostModel = mongoose.model('postsCintaNegra', PostSchema)
 
 // exporta un objeto de modelos
 module.exports = {
   UserModel,
+  PostModel,
 }
